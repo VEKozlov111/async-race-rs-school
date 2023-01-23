fc405f3faebc3d46c0b86e85607d7801066d3945
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-01-23 22:13:39 +0300

    feat: add storage

---
+++ async-race/src/components/storage/storage.ts
@@ -0,0 +1,10 @@
+export const storage = {
+  garagePage: constants.defaultGaragePage,
+  winnersPage: constants.defaultWinnersPage,
+  cars: [],
+  winners: [],
+  carsCount: 0,
+  winnersCount: 0,
+  sort: "time",
+  sortOrder: "asc",
+};
