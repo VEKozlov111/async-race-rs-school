6d06768d152ec5eedbbd1c32988ad37671a66047
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-01-23 22:26:08 +0300

    feat: add api file

---
+++ async-race/src/api/api.ts
@@ -0,0 +1,52 @@
+import { ICar, ICarCreate, ICarSpeed, IRace, IWinner } from "../types/types";
+
+
+export const getCars = async (
+  page: number,
+  limit = constants.garagePagesLimit
+): Promise<{ items: ICar[]; count: number }> => {
+  const response = await fetch(`${path.garage}?_page=${page}&_limit=${limit}`);
+  return {
+    items: await response.json(),
+    count: Number(response.headers.get("X-Total-Count")),
+  };
+};
+
+
+export const getCar = async (id: number): Promise<ICar> => {
+  return (await fetch(`${path.garage}/${id}`)).json();
+};
+
+export const updateCar = async (id: number, body: ICarCreate) =>
+  (
+    await fetch(`${path.garage}/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  ).json();
+
+export const createCar = async (body: ICarCreate): Promise<ICar> =>
+  (
+    await fetch(path.garage, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    })
+  ).json();
+
+export const deleteCar = async (id: number): Promise<void> => {
+  return (await fetch(`${path.garage}/${id}`, { method: "DELETE" })).json();
+};
+export const startEngine = async (id: number): Promise<ICarSpeed> =>
+  (await fetch(`${path.engine}?id=${id}&status=started`, { method: "PATCH" })).json();
+
+export const stopEngine = async (id: number): Promise<ICarSpeed> =>
+  (await fetch(`${path.engine}?id=${id}&status=stopped`, { method: "PATCH" })).json();
+
+export const drive = async (id: number): Promise<{ success: boolean }> => {
+  const res = await fetch(`${path.engine}?id=${id}&status=drive`, { method: "PATCH" }).catch();
+  return res.status !== 200 ? { success: false } : { ...(await res.json()) };
+};
